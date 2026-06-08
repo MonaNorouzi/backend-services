fb3dd19de8f01246a30739f3afbc8a1dcd78b00d
mona norouzi <monanorouzi85@gmail.com>
2026-06-08 23:46:07 +0330

    feat(api): implement REST API endpoints with inline serializers

--- timeviwe/urls.py
+++ timeviwe/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from . import apis
 
 urlpatterns = [
-    path("iran-time/", views.IranTimeView.as_view(), name="IR"),
-    path("country-time/", views.CountryTimeView.as_view(), name="country-time"),
-    path("signup/", views.RegisterView.as_view(), name="signup"),
-    path("signin/", views.LoginView.as_view(), name="signin"),
-    path("editprof/", views.EditProfileView.as_view(), name="editprof"),
+    path("iran-time/", apis.IranTimeView.as_view(), name="IR"),
+    path("country-time/", apis.CountryTimeView.as_view(), name="country-time"),
+    path("signup/", apis.RegisterView.as_view(), name="signup"),
+    path("signin/", apis.LoginView.as_view(), name="signin"),
+    path("editprof/", apis.EditProfileView.as_view(), name="editprof"),
 ]
