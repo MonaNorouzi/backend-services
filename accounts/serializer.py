899a764245dad1d7e385c6c5251d8f0ae5e1cd00
mona norouzi <monanorouzi85@gmail.com>
2026-06-22 16:51:09 +0330

    feat: know when you registered, you can see your username

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
+from rest_framework import serializers as ModelSerializer
+from accounts.models import User
 
-class UserRegisterserializer(serializers.Serializer):
+class UserRegisterserializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=100)
-    
-    password = serializers.CharField(write_only=True)
-
+    password = serializers.CharField(max_length=100, write_only=True)
+    class Meta:
+        model = User
+        fields = ('username','password')
 class UserProfileUpdateservices(serializers.Serializer):
     username = serializers.CharField(max_length=100, required=False)
     email = serializers.EmailField(required=False)
